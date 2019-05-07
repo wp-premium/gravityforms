e6c99f715f6c86671e0208e04539bd09c8dcaaa5
Gravity Forms <info@gravityforms.com>
2019-05-07 13:44:53 +0200

    2.4.8

--- gravityforms.php
+++ gravityforms.php
@@ -3,7 +3,7 @@
 Plugin Name: Gravity Forms
 Plugin URI: https://www.gravityforms.com
 Description: Easily create web forms and manage form entries within the WordPress admin.
-Version: 2.4.7.3
+Version: 2.4.8
 Author: rocketgenius
 Author URI: https://www.rocketgenius.com
 License: GPL-2.0+
@@ -215,7 +215,7 @@ class GFForms {
 	 *
 	 * @var string $version The version number.
 	 */
-	public static $version = '2.4.7.3';
+	public static $version = '2.4.8';
 
 	/**
 	 * Handles background upgrade tasks.
